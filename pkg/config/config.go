7cea5240380672fd4b193aee1a080dff9dd584ac
Marco Massenzio <1153951+massenz@users.noreply.github.com>
2025-04-29 00:12:23 -0700

    Implementing the per-project codestore (#21)
    This still resolves to the wrong folder
    
    Co-authored-by: Marco Massenzio <marco@alertavert.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -12,13 +12,22 @@ import (
 )
 
 const LocationEnv = "MAJORDOMO_CONFIG"
+const CodeLocationEnv = "MAJORDOMO_CODE"
 
 var DefaultConfigLocation = os.Getenv("HOME") + "/.majordomo/config.yaml"
+var DefaultCodeSnippetsLocation = os.Getenv("HOME") + "/.majordomo/code"
 
 type Project struct {
 	Name        string `yaml:"name" json:"name"`
 	Description string `yaml:"description,omitempty" json:"description,omitempty"`
 	Location    string `yaml:"location" json:"location"`
+	// User-configured location for the code snippets for this project.
+	// It is never overwritten by the system.
+	CodeSnippets string `yaml:"code_snippets,omitempty" json:"code_snippets,omitempty"`
+
+	// Resolved path for code snippets for the project.
+	// This is what the system uses, but is not written to the config file.
+	ResolvedCodeSnippetsDir string `yaml:"-" json:"-"`
 }
 
 // String function makes the Project type a valid fmt.Stringer
@@ -28,16 +37,17 @@ func (p Project) String() string {
 
 type Config struct {
 	// LoadedFrom is the path from which the Config was loaded.
-	LoadedFrom        string `yaml:"-"`
+	LoadedFrom string `yaml:"-"`
 
 	// OpenAIApiKey is the API key to use for OpenAI API requests.
-	OpenAIApiKey      string `yaml:"api_key"`
+	OpenAIApiKey string `yaml:"api_key"`
 
 	// ProjectId is the ID of the project to use for OpenAI API requests.
-	ProjectId         string `yaml:"project_id"`
+	ProjectId string `yaml:"project_id"`
 
 	// AssistantsLocation is the path to the YAML file containing the instructions
 	// to create the assistants' system prompts.
+	// TODO: not supported yet (see #18)
 	AssistantsLocation string `yaml:"assistants"`
 
 	// ThreadsLocation is the path to the directory where the threads are stored.
@@ -45,17 +55,17 @@ type Config struct {
 
 	// CodeSnippetsDir is the name of the directory, inside each respective
 	// project's location, where the code snippets are stored.
-	CodeSnippetsDir   string `yaml:"code_snippets"`
+	CodeSnippetsDir string `yaml:"code_snippets"`
 
 	// Model is the name of the model to use for OpenAI API requests.
-	Model             string `yaml:"model"`
+	Model string `yaml:"model"`
 
 	// ActiveProject is the name of the project that is currently active and will
 	// be used to fetch the files from (and save snippets to).
-	ActiveProject string    `yaml:"active_project"`
+	ActiveProject string `yaml:"active_project"`
 
 	// Projects is a list of projects that are configured in the system.
-	Projects      []Project `yaml:"projects"`
+	Projects []Project `yaml:"projects"`
 }
 
 // Save writes the Config to a YAML file at the given filePath.
@@ -104,6 +114,7 @@ func LoadConfig(filepath string) (*Config, error) {
 		return nil, fmt.Errorf("error unmarshaling yaml: %w", err)
 	}
 
+	// TODO: not having any projects configured should be a valid state.
 	if len(c.Projects) == 0 {
 		// TODO: we should have a default project.
 		return nil, fmt.Errorf("no projects configured")
@@ -118,6 +129,30 @@ func LoadConfig(filepath string) (*Config, error) {
 	}
 	if c.ActiveProject == "" && len(c.Projects) > 0 {
 		c.ActiveProject = c.Projects[0].Name
+	}
+	// The code snippets, if not configured is read from the environment.
+	if c.CodeSnippetsDir == "" {
+		c.CodeSnippetsDir = os.Getenv(CodeLocationEnv)
+		if c.CodeSnippetsDir == "" {
+			c.CodeSnippetsDir = DefaultCodeSnippetsLocation
+		}
+	}
+
+	// Resolve the actual code snippets directory for each project.
+	for i, _ := range c.Projects {
+		p := &c.Projects[i]
+		// By default, we use the global code snippets directory.
+		var cs = c.CodeSnippetsDir
+		if p.CodeSnippets != "" {
+			// If the project has a code snippets directory configured, we use that.
+			cs = p.CodeSnippets
+		}
+		if !path.IsAbs(cs) {
+			// If the path is not absolute, we assume it is relative to the project's location.
+			p.ResolvedCodeSnippetsDir = path.Join(p.Location, cs)
+		} else {
+			p.ResolvedCodeSnippetsDir = cs
+		}
 	}
 	return &c, nil
 }
